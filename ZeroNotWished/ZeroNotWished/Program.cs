87bad1917dafda5f340cfc491479be65415e82fb
Akira Kawamata <autumn@piedey.co.jp>
2021-01-29 19:00:46 +0900

    add ZeroNotWished

---
+++ ZeroNotWished/ZeroNotWished/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float n = 1.0f;
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine(1 / n);
+            n /= 1000.0f;
+        }
+    }
+}
